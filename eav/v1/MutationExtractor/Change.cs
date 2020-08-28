b670475657c8b1238f236ec477d26d5d9aa00f1b
jeroenwe <70137261+jeroenwe@users.noreply.github.com>
2020-08-28 16:03:21 +0200

    MutationExtractor - Updated DatabaseRepository to write mutations / changes to message queue. (#13)

--- eav/v1/MutationExtractor/Change.cs
+++ eav/v1/MutationExtractor/Change.cs
@@ -4,6 +4,28 @@ namespace MutationExtractor
 {
     public class Change
     {
-        public Guid Guid { get; } = Guid.NewGuid();
+        public int TenantId { get; set; }
+
+        public int EntityId { get; set; }
+
+        public DateTime? EntityStartDate { get; set; }
+
+        public DateTime? EntityEndDate { get; set; }
+
+        public bool EntityDeleted { get; set; }
+
+        public int TemplateId { get; set; }
+
+        public int MutationId { get; set; }
+
+        public int FieldId { get; set; }
+
+        public DateTime? StartDate { get; set; }
+
+        public DateTime? EndDate { get; set; }
+
+        public object Value { get; set; }
+
+        public bool IsDeleted { get; set; }
     }
 }
