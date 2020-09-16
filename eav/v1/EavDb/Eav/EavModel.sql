3a3e1fb64c32365fafa2b401689e6f84a2805af8
jeroenwe <70137261+jeroenwe@users.noreply.github.com>
2020-09-16 11:17:47 +0200

    Updated the Read API to take 'Is Deleted', Start date and End date into account while retrieving mutations for entities. (#19)
    Modified EAV Model and SQL script: entity ID's are no longer supposed to be auto-generated.
    
    Co-authored-by: Weerstra, Jeroen <jeroenwe@youforce.net>

--- eav/v1/EavDb/Eav/EavModel.sql
+++ eav/v1/EavDb/Eav/EavModel.sql
@@ -1,6 +1,6 @@
 IF EXISTS (SELECT * FROM sys.databases WHERE name = 'EavDB')
     BEGIN
-        DROP DATABASE EavDB
+        DROP DATABASE EavDb
     END
 GO 
 
@@ -35,8 +35,8 @@ GO
 -- Contains Entity instances, e.g. specific companies or employees.
 CREATE TABLE dbo.Entities
 (
-    Id INT IDENTITY(1,1) PRIMARY KEY,
-    ParentId INT,
+    Id INT PRIMARY KEY,
+    ParentId INT NOT NULL,
     Code VARCHAR(100) NOT NULL,  -- Custom code for the entity (contrary to the Id, which is technical key).
     Description VARCHAR(100),
     TemplateId INT FOREIGN KEY REFERENCES Templates(ID),
@@ -67,96 +67,11 @@ CREATE TABLE dbo.Mutations
     EntityId INT FOREIGN KEY REFERENCES Entities(ID),
     DataElementId INT FOREIGN KEY REFERENCES DataElements(ID),
     FieldValue VARCHAR(100),
-    StartDate DATETIME,
-    EndDate DATETIME,
-    Deleted BIT
+    StartDate DATETIME NOT NULL,
+    EndDate DATETIME NOT NULL,
+    Deleted BIT NOT NULL
 )
 GO
 
--- Insert entity types.
-INSERT INTO dbo.Templates
-(
-    Id, Name
-)
-VALUES
-    (12, 'Land'),
-    (15, 'Client'),
-    (17, 'Company'),
-    (21, 'Employee'),
-    (49, 'Contract')
-GO
-
--- Define a basic set of data elements.
-INSERT INTO dbo.DataElements
-(
-    Id, Description
-)
-VALUES
-    (10, 'Language'),
-    (22, 'Employee Code'),
-    (7014, 'Global Person Code'),
-    (524, 'Last Name'),
-    (24, 'Last Name At Birth'),
-    (26, 'Last Name At Birth Prefix'),
-    (51, 'First Names'),
-    (165, 'First Name To Use'),
-    (146, 'Formatted Name'),
-    (3000, 'Sort Name'),       
-    (25, 'Initials'),
-    (27, 'Partner Name'),
-    (166, 'Partner Name Prefix'),
-    (94, 'Title Prefix'),     -- E.g. Drs
-    (95, 'Title Suffix'),     -- E.g. MA
-    (35, 'Date Of Birth'),
-    (10302568, 'Date Of Death'),
-    (36, 'Gender'),
-    (37, 'Marital Status'),
-    (38, 'Employment Indicator'),  -- In/uitdienst
-    (39, 'Hire Date'),  -- Datum indienst
-    (308, 'First Hire Date'),  -- Eerste datum indienst
-    (10000018, 'National Identification Number'),
-    (40, 'End Date Employment'),
-    (10520479, 'Discharge Date'),  -- Geplande laatste datum indienst
-    (7212, 'Business Email Address'),
-    (7213, 'Private Email Address'),
-    (7374, 'Business Phone Number'),
-    (7376, 'Private Phone Number'),
-    (7377, 'Mobile Phone Number'),
-    (10204761, 'Nationality'),
-    -- Address fields
-    (391, 'Street Name'),
-    (392, 'Street Number'),
-    (393, 'Street Number Additional'),
-    (394, 'Postal Code'),
-    (395, 'City'),
-    (34,  'Country')
-
-GO
-
--- Insert a country, test clients (template 15) and some companies below them (template 17).
-INSERT INTO dbo.Entities
-(
-    ParentId, Code, Description, TemplateId, TenantId
-)
-VALUES
-    (NULL, 'NL', 'Nederland', 12, 0),
-    (1, '4024898', 'Metatech Nederland', 15, 2),
-    (1, '5031084', 'Volvo Nederland B.V.', 15, 3),
-    (2, '030', 'Metatech Administratie B.V.', 17, 2),
-    (2, '800', 'Metatech Constructie', 17, 2),
-    (2, '020', 'Metatech Horeca Services', 17, 2),
-    (2, '040', 'Metatech Wonen', 17, 2),
-    (3, 'DD41', 'Volvo Nederland B.V.', 17, 3),
-    (3, 'HH48', 'VFS Financial Services B.V.', 17, 3),
-    (3, 'HH55', 'Volvo Truck Center B.V.', 17, 3)
-GO
-
--- Insert initial Language value on country level.
-INSERT INTO dbo.Mutations
-(
-    EntityId, DataElementId, FieldValue, StartDate, EndDate
-)
-VALUES
-(
-    1, 10, 'NL', '2001-01-01', '9999-12-31'
-)
+-- See https://www.mssqltips.com/sqlservertip/1543/using-sqlcmd-to-execute-multiple-sql-server-scripts/
+:r /usr/src/app/Eav/TestData.sql
---
+++ eav/v1/EavDb/Eav/TestData.sql
@@ -0,0 +1,93 @@
+PRINT 'Inserting test clients, companies and data elements...'
+
+-- Insert entity types.
+INSERT INTO dbo.Templates
+(
+    Id, Name
+)
+VALUES
+    (12, 'Land'),
+    (15, 'Client'),
+    (17, 'Company'),
+    (21, 'Employee'),
+    (49, 'Contract')
+GO
+
+-- Define a basic set of data elements.
+INSERT INTO dbo.DataElements
+(
+    Id, Description
+)
+VALUES
+    (10, 'Language'),
+    (22, 'Employee Code'),
+    (7014, 'Global Person Code'),
+    (524, 'Last Name'),
+    (24, 'Last Name At Birth'),
+    (26, 'Last Name At Birth Prefix'),
+    (51, 'First Names'),
+    (165, 'First Name To Use'),
+    (146, 'Formatted Name'),
+    (3000, 'Sort Name'),       
+    (25, 'Initials'),
+    (27, 'Partner Name'),
+    (166, 'Partner Name Prefix'),
+    (94, 'Title Prefix'),     -- E.g. Drs
+    (95, 'Title Suffix'),     -- E.g. MA
+    (35, 'Date Of Birth'),
+    (10302568, 'Date Of Death'),
+    (36, 'Gender'),
+    (37, 'Marital Status'),
+    (38, 'Employment Indicator'),  -- In/uitdienst
+    (39, 'Hire Date'),  -- Datum indienst
+    (308, 'First Hire Date'),  -- Eerste datum indienst
+    (10000018, 'National Identification Number'),
+    (40, 'End Date Employment'),
+    (10520479, 'Discharge Date'),  -- Geplande laatste datum indienst
+    (7212, 'Business Email Address'),
+    (7213, 'Private Email Address'),
+    (7374, 'Business Phone Number'),
+    (7376, 'Private Phone Number'),
+    (7377, 'Mobile Phone Number'),
+    (10204761, 'Nationality'),
+    -- Address fields
+    (391, 'Street Name'),
+    (392, 'Street Number'),
+    (393, 'Street Number Additional'),
+    (394, 'Postal Code'),
+    (395, 'City'),
+    (34,  'Country')
+
+GO
+
+-- Insert a country, test clients (template 15) and some companies below them (template 17).
+INSERT INTO dbo.Entities
+(
+    Id, ParentId, Code, Description, TemplateId, TenantId, Deleted
+)
+VALUES
+    (10000001, 0, 'NL', 'Nederland', 12, 0),
+    (10029872, 10000001, '4024898', 'Metatech Nederland', 15, 10029872),
+    (10496315, 10000001, '5031084', 'Volvo Nederland N.V.', 15, 10496315),
+    (10618376, 10000001, '4000306', 'Vodafone Libertel', 15, 10618376),
+    (10028636, 10029872, '030', 'Metatech Administratie B.V.', 17, 10029872),
+    (10028637, 10029872, '800', 'Metatech Constructie', 17, 10029872),
+    (10028504, 10029872, '020', 'Metatech Horeca Services', 17, 10029872),
+    (10183634, 10029872, '040', 'Metatech Wonen', 17, 10029872),
+    (10496382, 10496315, 'DD41', 'Volvo Nederland B.V.', 17, 10496315),
+    (10496389, 10496315, 'HH48', 'VFS Financial Services B.V.', 17, 10496315),
+    (10496410, 10496315, 'HH55', 'Volvo Truck Center B.V.', 17, 10496315),
+    (10619998, 10618376, 'VL99', 'Vodafone Libertel B.V.', 17, 10618376)
+    
+GO
+
+-- Insert initial Language value on country level.
+INSERT INTO dbo.Mutations
+(
+    EntityId, DataElementId, FieldValue, StartDate, EndDate, Deleted
+)
+VALUES
+(
+    10000001, 10, 'NL', '2001-01-01', '9999-12-31', 0
+)
+GO
